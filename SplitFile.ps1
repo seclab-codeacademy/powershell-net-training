6fa686d15bceea7cba38d27e27c46e7a85f1e717
seclab-codeacademy <romain.seclab@gmail.com>
2016-02-26 07:35:52 +0100

    Added files via upload

--- SplitFile.ps1
+++ SplitFile.ps1
@@ -1,5 +1,5 @@
 <#
-Last Changed: 2016-02-25
+Last Changed: 2016-02-26
 
 powershell -ep bypass -f splitfile.ps1 -verbose
 
@@ -13,29 +13,33 @@ powershell -ep bypass -f splitfile.ps1 -verbose
 
 #>
 param (
-	[string]$inFilename    = "dark_psinfo.gif",
-	[int]$offSet           = 2281,
-	[string]$outFilename1  = "dark_part1.gif",
-	[string]$outFilename2  = "exploit.exe",
-	[switch]$verbose       = $false
+	[string]$inFile    = "dark_psinfo.gif",
+	[int]$offSet       = 2281,
+	[string]$outFile1  = "dark_part1.gif",
+	[string]$outFile2  = "exploit.exe",
+	[switch]$verbose   = $false
 )
 
+### MAIN ######################################################################
 
 if ($verbose) {
-	"In Filename  : $inFilename " | Write-Host 
+	"In Filename  : $inFile " | Write-Host 
 	"Offset       : 0x{0:X}  (Decimal: $offSet)" -f $offSet | Write-Host 
-	"Chunk part 1 : $outFilename1 " | Write-Host 
-	"Chunk part 2 : $outFilename2 " | Write-Host 
+	"Chunk part 1 : $outFile1 " | Write-Host 
+	"Chunk part 2 : $outFile2 " | Write-Host 
 }
 
-[byte[]]$Data = [IO.File]::ReadAllBytes($InFilename);
+if (Test-Path $inFile) {
+	[byte[]]$Data = [IO.File]::ReadAllBytes($InFile);
 
-$outstream1 = New-Object IO.FileStream $outFilename1, ([IO.FileMode]::Create); 
-$outstream1.Write($Data, 0, $offset); 
-$outstream1.Close(); 
+	$outstream1 = New-Object IO.FileStream $outFile1, ([IO.FileMode]::Create); 
+	$outstream1.Write($Data, 0, $offset); 
+	$outstream1.Close(); 
 
-$outstream2 = New-Object IO.FileStream $outFilename2, ([IO.FileMode]::Create); 
-$outstream2.Write($Data, $offset, $Data.length-$offset); 
-$outstream2.Close();
-
+	$outstream2 = New-Object IO.FileStream $outFile2, ([IO.FileMode]::Create); 
+	$outstream2.Write($Data, $offset, $Data.length-$offset); 
+	$outstream2.Close();
+} else {
+	Write-Warning "File '$inFile' not found" 
+}
 
