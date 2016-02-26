b37988395e5d5156d02a0f5d57aa409cbdce02e7
seclab-codeacademy <romain.seclab@gmail.com>
2016-02-26 07:35:33 +0100

    Added files via upload

--- md5hash.ps1
+++ md5hash.ps1
@@ -1,5 +1,5 @@
 <#
-Last Changed: 2016-02-25
+Last Changed: 2016-02-26
 
 powershell -ep bypass -f md5hash.ps1 -f md5hash.exe
 
@@ -8,24 +8,30 @@ param (
 	[string]$filename    = $null
 )
 
+### FUNCTION##################################################################
 
 function Get-MD5Hash($fileName) {
- if([System.IO.File]::Exists($fileName)) {
-  $fileStream = New-Object System.IO.FileStream($fileName,[System.IO.FileMode]::Open,[System.IO.FileAccess]::Read,[System.IO.FileShare]::ReadWrite)
-  $MD5Hash = New-Object System.Security.Cryptography.MD5CryptoServiceProvider
-  [byte[]]$fileByteChecksum = $MD5Hash.ComputeHash($fileStream)
-  $fileChecksum = ([System.Bitconverter]::ToString($fileByteChecksum)).Replace("-","")
-  $fileStream.Close()
- } else {
-  $fileChecksum = "ERROR: $fileName Not Found"
- }
- return $fileChecksum
+	if([System.IO.File]::Exists($fileName)) {
+		$fileStream = New-Object System.IO.FileStream($fileName,[System.IO.FileMode]::Open,[System.IO.FileAccess]::Read,[System.IO.FileShare]::ReadWrite)
+		$MD5Hash = New-Object System.Security.Cryptography.MD5CryptoServiceProvider
+		[byte[]]$fileByteChecksum = $MD5Hash.ComputeHash($fileStream)
+		$fileChecksum = ([System.Bitconverter]::ToString($fileByteChecksum)).Replace("-","")
+		$fileStream.Close()
+	} else {
+		$fileChecksum = "ERROR: $fileName Not Found"
+	}
+
+	return $fileChecksum
 }
 
 
-### MAIN ###############################################33
+### MAIN #####################################################################
 
 
-$md5 = Get-MD5Hash $filename
+if (Test-Path $filename) {
+	$md5 = Get-MD5Hash $filename
 
-Write-Host "$md5 -- $filename"
+	Write-Host "$md5 -- $filename"
+} else {
+	Write-Warning "File '$filename' not found" 
+}
