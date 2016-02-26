63d20cb215d5017c0ada6e35f25a59bb253920b7
seclab-codeacademy <romain.seclab@gmail.com>
2016-02-26 07:35:19 +0100

    Added files via upload

--- patchme.ps1
+++ patchme.ps1
@@ -1,19 +1,22 @@
 <#
-Last Changed: 2016-02-25
+Last Changed: 2016-02-26
 
-powershell -ep bypass -f patchme.ps1 -f cmd_disabled.exe  -offset 0x526C -value 0x42 -verbose
+### Windows 7 32BIT Offset: 21100 (0x526C)
+powershell -ep bypass -f patchme.ps1 -f cmd_disabled.exe  -offset 0x526C -value 0x21 -verbose
 
+### Windows 7 64BIT Offset: 164474 (0x02827A)
+powershell -ep bypass -f patchme.ps1 -f cmd_disabled.exe  -offset 0x02827A -value 0x21 -verbose
+ 
 #>
 param (
-	[string]$filename  = "cmd_disabled.exe",
+	[string]$filename  = $null,
 	[int]$offSet       = 0x526C,
-	[int]$value        = 0x42,
+	[int]$value        = 0x21,
 	[switch]$verbose   = $false
 )
 
 
 ### MAIN ################################################
-
 
 if ($verbose) {
 	[byte[]]$Data = [IO.File]::ReadAllBytes($filename)
@@ -21,11 +24,14 @@ if ($verbose) {
 	'Old Value  : 0x{0:X}' -f $Data[$offSet] | Write-Host 
 }
 
-
-[byte[]]$Data = [IO.File]::ReadAllBytes($filename)
-$Data[$offSet]= [byte]$value
-[IO.File]::WriteAllBytes($filename, $Data) 
-
+if (Test-Path $filename) {
+	[byte[]]$Data = [IO.File]::ReadAllBytes($filename)
+	$Data[$offSet]= [byte]$value
+	[IO.File]::WriteAllBytes($filename, $Data) 
+} else {
+	Write-Warning "File '$filename' not found"
+	exit
+}
 
 if ($verbose) {
 	[byte[]]$Data = [IO.File]::ReadAllBytes($filename)
